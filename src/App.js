3e18f9f5fdad9df2a09eb0e8e002a2660002c9be
Tharshan Ketheeswaran <tketheeswaran09@gmail.com>
2020-05-12 23:01:07 +0100

    done proj

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 //TODO: STEP 1 - Import the useState hook.
-import React from "react";
-import BottomRow from "./BottomRow";
+import React, { useState } from "react";
 import "./App.css";
+import BottomRow from "./BottomRow";
 
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
+ 
+  const [homeScore, homeSetScore] = useState(0);
+  const [awayScore, awaySetScore] = useState(0);
+
+
+  //adding handler function
+  function handler(team, score) {
+    if (team === 'home') {
+      homeSetScore(homeScore + score);
+    } else {
+      awaySetScore(awayScore + score);
+    }
+  }
+
+
 
   return (
     <div className="container">
@@ -13,13 +28,16 @@ function App() {
           <div className="home">
             <h2 className="home__name">Lions</h2>
 
-            {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
-            <div className="home__score">32</div>
+            {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept 
+            dynamic values from our state. */}
+
+
+            <div className="home__score">{homeScore}</div>
           </div>
           <div className="timer">00:03</div>
           <div className="away">
             <h2 className="away__name">Tigers</h2>
-            <div className="away__score">32</div>
+            <div className="away__score">{awayScore}</div>
           </div>
         </div>
         <BottomRow />
@@ -28,12 +46,21 @@ function App() {
         <div className="homeButtons">
 
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-          <button className="homeButtons__touchdown">Home Touchdown</button>
-          <button className="homeButtons__fieldGoal">Home Field Goal</button>
+
+          <button onClick={() => handler('home', 7)}
+            className="homeButtons__touchdown">Home Touchdown</button>
+
+          <button onClick={() => handler('home', 3)}
+            className="homeButtons__fieldGoal">Home Field Goal</button>
+          <button onClick={() => homeSetScore(0)} className="reset_button">Reset HomeScore</button>
         </div>
         <div className="awayButtons">
-          <button className="awayButtons__touchdown">Away Touchdown</button>
-          <button className="awayButtons__fieldGoal">Away Field Goal</button>
+          <button onClick={() => handler('away', 7)}
+            className="awayButtons__touchdown">Away Touchdown</button>
+
+          <button onClick={() => handler('away', 3)}
+            className="awayButtons__fieldGoal">Away Field Goal</button>
+          <button onClick={() => awaySetScore(0)} className="reset_button">Reset AwayScore</button>
         </div>
       </section>
     </div>
@@ -41,3 +68,4 @@ function App() {
 }
 
 export default App;
+
